538101eaf3ccf78d96b64be1ebae9ba006bd7b57
pragatitalekar <pragatitalekar31@gmail.com>
2025-10-17 16:52:17 +0530

    Initial commit

---
+++ src/test/java/com/products/pragati/ProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.products.pragati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
